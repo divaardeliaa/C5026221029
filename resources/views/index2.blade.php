11cb0eb27b457165705982aa5db6e8e9289d5ec9
divaardeliaa <ardeliadiva99@gmail.com>
2024-04-08 12:00:47 +0700

    fix salah push

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -186,9 +186,12 @@
                                         <li data-target="#demo" data-slide-to="0" class="active"></li>
                                         <li data-target="#demo" data-slide-to="1"></li>
                                         <li data-target="#demo" data-slide-to="2"></li>
+<<<<<<< HEAD
                                         <li data-target="#demo" data-slide-to="3"></li>
                                         <li data-target="#demo" data-slide-to="4"></li>
                                         <li data-target="#demo" data-slide-to="5"></li>
+=======
+>>>>>>> parent of d420e00 (update portofolio)
                                     </ul>
 
                                     <!-- The slideshow -->
@@ -204,6 +207,7 @@
                                         <div class="carousel-item">
                                             <img src="img6.jpg" alt="New York">
                                         </div>
+<<<<<<< HEAD
                                         <div class="carousel-item">
                                             <img src="img7.jpg" alt="paris">
                                         </div>
@@ -213,6 +217,8 @@
                                         <div class="carousel-item">
                                             <img src="img10.jpeg" alt="hihi">
                                         </div>
+=======
+>>>>>>> parent of d420e00 (update portofolio)
                                     </div>
                                         <!-- Left and right controls -->
                                         <a class="carousel-control-prev" href="#demo" data-slide="prev">
